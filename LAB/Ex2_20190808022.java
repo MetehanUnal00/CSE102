4e80f8679e37606c5313e4ac935b18163ecdef26
Metehan Ünal <metehan2412@gmail.com>
2023-03-29 12:08:20 +0300

    Now we commit second and third exercise.

---
+++ LAB/Ex2_20190808022.java
@@ -0,0 +1,82 @@
+/**
+ * @author Metehan Ünal
+ * @date 29.03.2023
+ * @version 1.0
+ */
+public class Ex2_20190808022{
+    public static void main(String[] args){
+
+    }
+}
+class Ticket{
+    private City from;
+    private City to;
+    private java.util.Date date;
+    private int seat;
+    public Ticket (City from, City to, java.util.Date date, int seat){
+        this.from = from;
+        this.to = to;
+        this.date = date;
+        this.seat = seat;
+    }
+    public Ticket(City from, City to, int seat) {
+        this.from = from;
+        this.to = to;
+        this.date = new java.util.Date(System.currentTimeMillis()+86400000);
+        this.seat = seat;
+    }
+    public City getFrom() {
+        return from;
+    }
+    public City getTo() {
+        return to;
+    }
+    public java.util.Date getDate() {
+        return date;
+    }
+    public int getSeat() {
+        return seat;
+    }
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+    public void setDate(java.util.Date date) {
+        this.date = date;
+    }
+}
+class City{
+    private String postalCode;
+    private String name;
+    public City(String postalCode, String name){
+        this.postalCode = postalCode;
+        this.name = name;
+    }
+    public String getPostalCode() {
+        return postalCode;
+    }
+    public String getName() {
+        return name;
+    }
+}
+class Person{
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    public Person(String name, String surname, String phoneNumber){
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
